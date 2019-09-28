c2a17ddf898486ff96256174e15b9fbc423d28d3
PanumartFirm <pspisozsw2@hotmail.com>
2019-09-28 11:38:20 +0700

    update delete

--- app.js
+++ app.js
@@ -15,15 +15,46 @@ function createtodos (data){
         todos.push(todo)
         return todo
 }
+function deleteitem(id){
+    delete todos[id-1]
+}
 app.get('/todos',(req,res)=>{
     res.send(todos)
 })
+app.get('/todos/:id',(req,res)=> {
+        let id = req.params.id
+        let result = todos.filter(todo => todo.id.toString() === id )
+        let todo = result[0]
+        res.send(todo)
 
+})
+app.post('/todos/:id',(req,res) =>{
+        let id = req.params.id
+        let result = todos.filter(todo =>todo.id.toString() === id)
+        let todo =result[0]
+        if (todo.completed === false){
+            
+        }
+
+})
 app.post('/todos',(req,res)=>{
         let todo = createtodos(req.body.title)   
         res.send(todo).sendStatus(201)  
 })
-
+app.delete('/todos/:id',(req,res)=>{
+        let id = req.params.id 
+        // delete todos [id-1]   //ลบแบบปกติแต่จะมีช่องว่าง
+        let result = todos.filter(todo =>todo.id.toString() !== id)
+        todos = result
+        // console.log(todos)  // ตรวจสอบว่าคำตอบตรงไหม
+        res.sendStatus(204)
+})
+app.put('/todos/:id', (req,res) => {
+    let id = req.params.id
+    let todo = todos.find(t=>t.id.toString() === id)
+    console.log('todo '+todo.id)
+    res.send(todo)
+})
 app.listen(port,()=>{
     console.log("Todo API Server stared at "+port)
 })
