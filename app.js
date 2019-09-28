f33b70f32655b4fb6b6c11c2d68957de53d6aed8
PanumartFirm <pspisozsw2@hotmail.com>
2019-09-28 10:11:12 +0700

    Add method post and function createtodo

--- app.js
+++ app.js
@@ -8,11 +8,21 @@ let todos = [
     {id:2, title: 'Completed Sample todo', completed: true}
 ]
 
-app.get('/',(req,res)=>{
+
+function createtodos (data){
+        let title = data
+        let todo = {title: title,completed: false, id: todos.length+1}
+        todos.push(todo)
+        return todo
+}
+app.get('/todos',(req,res)=>{
     res.send(todos)
 })
 
-
+app.post('/todos',(req,res)=>{
+        let todo = createtodos(req.body.title)   
+        res.send(todo).sendStatus(201)  
+})
 
 app.listen(port,()=>{
     console.log("Todo API Server stared at "+port)
