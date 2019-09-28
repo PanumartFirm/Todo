d8853ecacb6cf7f5a3da5de465b2f384e09fdc1a
PanumartFirm <pspisozsw2@hotmail.com>
2019-09-28 09:50:13 +0700

    second commit

--- app.js
+++ app.js
@@ -3,7 +3,14 @@ const app = express()
 const port  = 3000
 app.use(express.json())
 
+let todos = [
+    {id:1, title: 'Sample todo', completed: false},
+    {id:2, title: 'Completed Sample todo', completed: true}
+]
 
+app.get('/',(req,res)=>{
+    res.send(todos)
+})
 
 
 
